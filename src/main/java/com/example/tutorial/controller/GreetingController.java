b8e6c9ef53852e5658ed875a2f73c9ae25b8d944
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-11 00:50:39 -0400

    initial commit

---
+++ src/main/java/com/example/tutorial/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.tutorial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String getGreeting() {
+        return "Hello Java Spring!";
+    }
+}
